6d7e86779b6ec28918beb76277d28fc79146ccd2
yebin <na04232@naver.com>
2023-12-03 16:53:47 +0900

    Creating a Window

---
+++ HelloOpenTK/MyGameWindow.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+namespace HelloOpenTK
+{
+	public class MyGameWindow : GameWindow
+	{
+        public MyGameWindow(int width, int height, string title) : base(GameWindowSettings.Default, new NativeWindowSettings() { Size = (width, height), Title = title }) { }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
+
+            if (KeyboardState.IsKeyDown(Keys.Escape))
+            {
+                Close();
+            }
+        }
+
+    }
+}
+
--- HelloOpenTK/Program.cs
+++ HelloOpenTK/Program.cs
@@ -1,9 +1,18 @@
 ﻿using System;
+using OpenTK;
+using OpenTK.Windowing.Desktop;
+using HelloOpenTK;
 
 public class Program
 {
     static void Main(String[] args)
     {
         Console.WriteLine("Hello ! OpenTk!");
+
+        using (MyGameWindow game= new MyGameWindow(800, 600, "LearnOpenTK"))
+        {
+            game.Run();
+        }
     }
+
 }
