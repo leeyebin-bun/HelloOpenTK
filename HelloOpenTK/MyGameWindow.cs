bc37075664f91033a1be11bd6090c0ade04d07a0
yebin <na04232@naver.com>
2023-12-09 16:11:58 +0900

    HelloTriangle

--- HelloOpenTK/MyGameWindow.cs
+++ HelloOpenTK/MyGameWindow.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using OpenTK.Graphics.OpenGL;
 using OpenTK.Windowing.Common;
 using OpenTK.Windowing.Desktop;
 using OpenTK.Windowing.GraphicsLibraryFramework;
@@ -9,6 +10,16 @@ namespace HelloOpenTK
 	{
         public MyGameWindow(int width, int height, string title) : base(GameWindowSettings.Default, new NativeWindowSettings() { Size = (width, height), Title = title }) { }
 
+        float[] vertices = {
+             -0.5f, -0.5f, 0.0f, //Bottom-left vertex
+              0.5f, -0.5f, 0.0f, //Bottom-right vertex
+              0.0f,  0.5f, 0.0f  //Top vertex
+              };
+
+        Shader shader;
+
+        int VertexArrayObject;
+
         protected override void OnUpdateFrame(FrameEventArgs e)
         {
             base.OnUpdateFrame(e);
@@ -17,6 +28,57 @@ namespace HelloOpenTK
             {
                 Close();
             }
+        }
+
+        protected override void OnLoad()
+        {
+            base.OnLoad();
+            GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);//Code goes here
+
+            ///////////// VBO
+            int VertexBufferObject;
+            // 그래픽카드에 Buffer(저장공간) 을 생성
+            VertexBufferObject = GL.GenBuffer();
+            // 생성한 Buffer를 어떤 특성(ArrayBuffer)으로 선택
+            GL.BindBuffer(BufferTarget.ArrayBuffer, VertexBufferObject);
+            // 선택한 Buffer에 데이터를 보내기
+            GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
+
+            ///////////// SHADER
+            string ProjectDirectory = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
+            string vertexPath = Path.Join(ProjectDirectory, "Vertex.glsl").ToString();
+            string fragmentPath = Path.Join(ProjectDirectory, "Fragment.glsl").ToString();
+
+            shader = new Shader(vertexPath, fragmentPath);
+
+            ///////////// VAO
+            VertexArrayObject = GL.GenVertexArray();
+            // 그래픽카드에 VertexArrayobject를 저장할 공간 할당
+            GL.BindVertexArray(VertexArrayObject);
+            // VertexArrayobject를 선택
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+            // 그래픽카드에 Bind 된 Buffer의 해석 방법을 Bind된 VertexArrayobject에 저장
+            // 0번 속성 , 데이터3개 , float 타입
+            GL.EnableVertexAttribArray(0);
+            // 0번 속성을 Enable
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+            GL.Clear(ClearBufferMask.ColorBufferBit);//Code goes here.
+
+            shader.Use();
+            GL.BindVertexArray(VertexArrayObject);
+            GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
+
+            SwapBuffers();
+        }
+
+        protected override void OnResize(ResizeEventArgs e)
+        {
+            base.OnResize(e);
+            GL.Viewport(0, 0, e.Width, e.Height);
         }
 
     }
